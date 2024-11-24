243e8ca857bb6447911fb5ef22900cf1dc7b0391
alanandrewvarghese <alanandrew883@gmail.com>
2024-11-24 13:08:23 +0530

    Add graph updation with custom time

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,60 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, request
 from app import create_app  # Import the create_app function
 from app import mysql
+from datetime import datetime, timezone, timedelta
 import pandas as pd
 import plotly.express as px
+import pytz
 
 def init_routes(app):
+
+    IST = pytz.timezone('Asia/Kolkata')
+
     @app.route('/')
     def index():
 
         try:
             cur = mysql.connection.cursor()
-            cur.execute('SELECT * FROM power_readings')
+            cur.execute('SELECT * FROM measurements')
             chart_data = cur.fetchall()
-            print(chart_data)
         except Exception as e:
             print(e)
 
         columns = [desc[0] for desc in cur.description]
         df = pd.DataFrame(chart_data, columns=columns)
         
-        fig = px.line(df, x="date", y="power_usage", title="Power Usage Over Time")
+        fig = px.line(df, x="timestamp", y="power", title="Power Usage Over Time")
         chart_html = fig.to_html(full_html=False)  # Convert Plotly chart to HTML
     
         return render_template('index.html', chart=chart_html, df=df)
+    
+    @app.route("/update_graph",methods=["POST"])
+    def update_graph():
+        timestamp_start = request.form.get("timestamp_start")
+        timestamp_end = request.form.get("timestamp_end")
+
+        if not timestamp_start:
+            timestamp_start = datetime.now(timezone.utc) - timedelta(hours=6)
+            timestamp_start = timestamp_start.astimezone(IST)
+
+        if not timestamp_end:
+            timestamp_end = datetime.now(timezone.utc)  # Get the current time in UTC and make it timezone-aware
+            timestamp_end = timestamp_end.astimezone(IST)
+
+        query = f"select * from measurements where timestamp > '{timestamp_start}' and timestamp < '{timestamp_end}' limit 86400;"
+
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            chart_data = cur.fetchall()
+        except Exception as e:
+            print(e)
+
+        columns = [desc[0] for desc in cur.description]
+        df = pd.DataFrame(chart_data, columns=columns)
+        
+        fig = px.line(df, x="timestamp", y="power", title="Power Usage Over Time")
+        chart_html = fig.to_html(full_html=False)  # Convert Plotly chart to HTML
+    
+        return render_template('index.html', chart=chart_html, df=df)
+    
--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
 
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'root'
-    MYSQL_DB = 'pm'
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
